eecbbf318181ccdefa1d2a04e46ade7a834fdcd3
Majock Bim <162757080+majockbim@users.noreply.github.com>
2025-11-02 17:56:44 -0500

    Time: 4 ms (68.01%) | Memory: 21.3 MB (39.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,6 +1,5 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        from collections import Counter
 
         # Returns a 'dictionary' like {1: 3, 2: 2}
         counts = Counter(nums)

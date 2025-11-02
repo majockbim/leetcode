7208bc436f38caf7ca431560648e716056e18272
Majock Bim <162757080+majockbim@users.noreply.github.com>
2025-11-02 17:56:11 -0500

    Time: 0 ms (100.00%) | Memory: 21.3 MB (39.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,9 +2,12 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         from collections import Counter
 
+        # Returns a 'dictionary' like {1: 3, 2: 2}
         counts = Counter(nums)
+        # Returns a nested list of most common items, [[1,3],[2,2]]
         m = counts.most_common(k)
 
+        # add most common digits
         r = []
         for item in m:
             r.append(item[0])

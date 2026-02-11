2795fbf2d2ed26b256f1a115c3eb4835f2971acd
Majock Bim <162757080+majockbim@users.noreply.github.com>
2026-02-10 21:59:43 -0500

    Time: 0 ms (100.00%) | Memory: 16.9 MB (41.46%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        std::vector<int> res = nums;
-        for(int n : nums) {
-            res.push_back(n);
+        int n = nums.size();
+
+        vector<int> res;
+
+        // prevent reallocation of memory
+        res.reserve(2*n);
+
+        for(int i=0; i<2*n; ++i) {
+            res.push_back(nums[i % n]);
         }
+
         return res;
+
     }
 };

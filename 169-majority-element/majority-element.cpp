9d1fd83fa9e4e03c663446ce604d56ddf46d1b98
Majock Bim <162757080+majockbim@users.noreply.github.com>
2026-02-19 22:07:30 -0500

    Time: 0 ms (100.00%) | Memory: 28 MB (86.38%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> count;
+        int curr = 0;
+        int count = 0;
+        for(int n : nums) {
 
-        for(int i = 0; i < nums.size(); ++i) {
-            count[nums[i]]++;
-        }
-
-        int max = 0;
-        int res = 0;
-        for(auto &pair : count) {
-            if(pair.second > max) {
-                max = pair.second;
-                res = pair.first;
+            if(count == 0) {
+                curr = n;
             }
+
+            if(n == curr) count++;
+            else count--;
         }
-        return res;
+        return curr;
     }
 };
